9e8245447f80a656b7da9f7f347746c918dd6182
isabroch <isabrochweb@gmail.com>
2020-02-11 12:40:50 +0100

    trigger new error

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function add(a, b) {
-  return a + b + c;
-}
+// ! REFERENCE ERRORS
+// error one
+let refError = undefinedVariable;
 
-add(5, 4, 3)
+// error two
+(function add(a, b) {
+  return a + b + c;
+})(5,4,3)
+
+//error three
+let number;
+for (let index = 0; index < 5; index++) {
+  number = index;
+}
+number = index;
