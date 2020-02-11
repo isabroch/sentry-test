59887f61d327e3c639621cdeee5830f77effc86a
isabroch <isabrochweb@gmail.com>
2020-02-11 12:48:37 +0100

    add new reference  error

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ for (let index = 0; index < 5; index++) {
   number = index;
 }
 number = index;
+
+// error four
+(function () {
+  const a = 1;
+  const b = 2;
+  return a + b;
+})()
+console.log(a);
+
+// error five
+const call = {
+  to: "61121066",
+  from: "99004858",
+  showDetails: () => {
+    const message = `${this.from} is calling ${this.to}`;
+    return message;
+  }
+}
+call.showDetails();
