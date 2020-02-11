9a88e49f29377d3e2cc36362c1883d13d8e2c426
isabroch <isabrochweb@gmail.com>
2020-02-11 13:05:14 +0100

    test error 5

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ number = index;
 const call = {
   to: "61121066",
   from: "99004858",
-  showDetails: () => {
+  showDetails: function() {
     const message = `${this.from} is calling ${this.to}`;
     return message;
   }
@@ -35,8 +35,8 @@ const call = {
 call.showDetails();
 
 // error six
-console.log(runFunction());
-const runFunction = () => { return 'Does this exist yet?'}
+// console.log(runFunction());
+// const runFunction = () => { return 'Does this exist yet?'}
 
 // error seven
 document.querySelector('#error').addEventListener('click', (e) => {
