682a2c3e26a160f8e2d9b53921146e8f40104f14
isabroch <isabrochweb@gmail.com>
2020-02-11 12:49:04 +0100

    trigger new error

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let refError = undefinedVariable; */
   return a + b + c;
 })(5,4,3) */
 
-//error three
+/* //error three
 let number;
 for (let index = 0; index < 5; index++) {
   number = index;
 }
-number = index;
+number = index; */
 
 // error four
 (function () {
