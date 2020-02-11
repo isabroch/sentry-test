1b709abe47e642554a92cca678c5f2ead975519f
isabroch <isabrochweb@gmail.com>
2020-02-11 11:21:32 +0100

    log error to console

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ try {
   let typeError = document.querySelector('#doesNotExist');
   typeError.innerHTML = "This doesn't exist though!";
 } catch (error) {
+  console.error(error);
   document.querySelector('#error').innerHTML = error.message;
 }
