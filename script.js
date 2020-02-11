088c29af187264e838e38ed290b161bec0d01a83
isabroch <isabrochweb@gmail.com>
2020-02-11 12:51:58 +0100

    create error scenarios

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 // error one
 let refError = undefinedVariable;
 
-/* // error two
+// error two
 (function add(a, b) {
   return a + b + c;
-})(5,4,3) */
+})(5,4,3)
 
-/* //error three
+//error three
 let number;
 for (let index = 0; index < 5; index++) {
   number = index;
 }
-number = index; */
+number = index;
 
-/* // error four
+// error four
 (function () {
   const a = 1;
   const b = 2;
   return a + b;
 })()
-console.log(a); */
+console.log(a);
 
-/* // error five
+// error five
 const call = {
   to: "61121066",
   from: "99004858",
@@ -31,4 +31,4 @@ const call = {
     return message;
   }
 }
-call.showDetails(); */
+call.showDetails();
